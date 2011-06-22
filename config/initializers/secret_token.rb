a00b9e6af7d0351a7ea8f1947daff2bfb144bf89
Will Dennis <williamldennis@gmail.com>
2011-06-22 13:46:03 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spinlister::Application.config.secret_token = '85054a2b113f8d2aada8e0ec9246bc9b2ca26c6c404b5da49f4fe4c19d03a9b59e17d71705f757c9489ed1d01bbaf96b48e9abb4ea1166ab065a87d839341e58'
