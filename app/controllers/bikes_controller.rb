2a079850e8058553c72c7d870cf5c0e246b35e74
Will Dennis <williamldennis@gmail.com>
2011-07-07 21:38:18 -0400

    created common error partial and (almost) working list your bike

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,17 +1,26 @@
 class BikesController < ApplicationController
+
+  before_filter :authenticate
+  
   def listyourbike 
-     @bike = bike.user
+     @bike = Bike.new if signed_in?
      @title = "List Your Bike"
    end
    
    def create
-     @bike = Bike.new
-     redirect_to mybikes_path, :flash => { :success => "Your Bike is Listed!" }
+     @bike = current_user.bikes.build(params[:bike])
+      if @bike.save
+        redirect_to mybikes_path, :flash => { :success => "Your Bike is Listed!" }
+      else
+        render 'bikes/listyourbike'
+      end
+   end
+   
+   def destroy
    end
    
    def show
      @bike = Bike.find(params[:id])
      @title = @bike.name
    end
-   
 end
--- spec/controllers/bikes_controller_spec.rb
+++ spec/controllers/bikes_controller_spec.rb
@@ -1,5 +1,74 @@
 require 'spec_helper'
 
 describe BikesController do
-
+  render_views
+  
+  describe "access control" do
+  
+    it "should deny access to 'create'" do
+      post :create
+      response.should redirect_to(signin_path)
+    end
+  
+    it "should deny access to 'destroy'" do
+      delete :destroy, :id => 1
+      response.should redirect_to(signin_path)
+    end
+  end
+  
+  describe "POST 'create'" do
+    
+    before(:each) do
+      @user = test_sign_in(Factory(:user))
+    end
+    
+    describe "failure" do
+      
+      before(:each) do
+        @attr = { :name => " ",
+                  :size => " ",
+                  :description => " ",
+                  :biketype => " ",
+                  :price => " "}
+      end 
+      
+      it "should not create a bike listing" do
+        lambda do
+          post :create, :bike => @attr
+        end.should_not change(Bike, :count)
+      end
+      
+      it "should re-render the list your bike page" do
+        post :create, :bike => @attr
+        response.should render_template('bikes/listyourbike')
+      end
+    end
+    
+    describe "success" do
+      
+      before(:each) do
+        @attr = {  :name => "Sweet Ass Bike",
+                  :size => "6 feet",
+                  :description => " Ride her with respect. She's been good to me.",
+                  :biketype => "Vintage Roadie ",
+                  :price => "13.50"}
+      end
+      
+      it "should create a bike listing" do
+        lambda do
+          post :create, :bike => @attr
+        end.should change(Bike, :count).by(1)
+      end
+      
+      it "should redirect to the profile page" do
+        post :create, :bike => @attr
+        response.should redirect_to(mybikes_path)
+      end
+      
+      it "should have a flash success message" do
+        post :create, :bike => @attr
+        flash[:success].should =~ /your bike is listed!/i
+      end
+    end
+  end
 end
