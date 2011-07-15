696d8629d76c698f700eb45aa3f0cb3a26dea8cc
Will Dennis <williamldennis@gmail.com>
2011-07-15 19:10:20 -0400

    changed font size and corrected location fields

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -15,7 +15,7 @@ class BikesController < ApplicationController
    def index 
      @title = "Bikes"
      if params[:search]
-       @bikes = Bike.find(:all, :conditions => ['LOWER (city) LIKE ?', "%#{params[:search.downcase]}%"]).paginate(:page => params[:page], :per_page => 2)
+       @bikes = Bike.find(:all, :conditions => ['LOWER (address) LIKE ?', "%#{params[:search.downcase]}%"]).paginate(:page => params[:page], :per_page => 2)
      else
        @bikes = Bike.paginate(:page => params[:page], :per_page => 2)
      end
