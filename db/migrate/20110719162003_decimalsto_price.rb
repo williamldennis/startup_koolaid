f6f6579f68780b9473c2ad6d84c03fd017bf4bf9
Will Dennis <williamldennis@gmail.com>
2011-07-19 18:59:10 -0400

    trying to update heroku db due to precision/decimal issue

--- db/migrate/20110719162003_decimalsto_price.rb
+++
@@ -1,8 +0,0 @@
-class DecimalstoPrice < ActiveRecord::Migration
-  def self.up
-    change_column :bikes, :price, :decimal, :scale => 2
-  end
-
-  def self.down
-  end
-end
--- db/migrate/20110719162549_decimalsto_price_change.rb
+++
@@ -1,8 +0,0 @@
-class DecimalstoPriceChange < ActiveRecord::Migration
-  def self.up
-    remove_column :bikes, :price, :decimal, :scale => 2
-  end
-
-  def self.down
-  end
-end
