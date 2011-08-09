450a3da0e381418f2478fd88830f2a61f03983af
Will Dennis <williamldennis@gmail.com>
2011-08-09 14:49:41 -0400

    trying to get heroku to work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110802021346) do
+ActiveRecord::Schema.define(:version => 20110809005925) do
+
+  create_table "assets", :force => true do |t|
+    t.integer  "bike_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "bikes", :force => true do |t|
     t.string   "name"
@@ -32,6 +42,11 @@ ActiveRecord::Schema.define(:version => 20110802021346) do
     t.float    "longitude"
     t.decimal  "price",              :precision => 8, :scale => 2
     t.string   "neighborhood"
+    t.string   "phone_number"
+    t.text     "bike_imperfections"
+    t.string   "lock_type"
+    t.text     "reviews"
+    t.string   "pick_up_type"
   end
 
   add_index "bikes", ["user_id"], :name => "index_bikes_on_user_id"
@@ -77,13 +92,24 @@ ActiveRecord::Schema.define(:version => 20110802021346) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "user_created_at"
+    t.datetime "user_updated_at"
     t.string   "encrypted_password"
     t.string   "salt"
     t.boolean  "admin"
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",        :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
