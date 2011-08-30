e0d735aca21054213aacc4e824bf0696243e31d6
Will Dennis <williamldennis@gmail.com>
2011-08-30 11:19:51 -0700

    removed password protection and added google analytics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
-  
-  USER_NAME, PASSWORD = "campusbeta", "drinks"
-  before_filter :authenticate
+  # 
+  # USER_NAME, PASSWORD = "campusbeta", "drinks"
+  # before_filter :authenticate
   
   filter_parameter_logging :card_number, :card_verification
 
   private
-    def authenticate
-      authenticate_or_request_with_http_basic do |user_name, password|
-        user_name == USER_NAME && password == PASSWORD
-      end
-    end
+    # def authenticate
+    #   authenticate_or_request_with_http_basic do |user_name, password|
+    #     user_name == USER_NAME && password == PASSWORD
+    #   end
+    # end
     
     def current_cart
       Cart.find(session[:cart_id])
